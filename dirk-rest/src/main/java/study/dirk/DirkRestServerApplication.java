02c7368628caa5a2db75b17ddd40585dbc296484
wuti <linshi100q@163.com>
2019-05-16 17:09:40 +0800

    eureka

---
+++ dirk-rest/src/main/java/study/dirk/DirkRestServerApplication.java
@@ -0,0 +1,12 @@
+package study.dirk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirkRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirkRestServerApplication.class, args);
+	}
+}

7e6b958b3edd378d8c3a69e048d06202b78e4db5
wuti <linshi100q@163.com>
2019-06-01 12:57:04 +0800

    hystrix

--- dirk-rest/src/main/java/study/dirk/DirkRestController.java
+++ dirk-rest/src/main/java/study/dirk/DirkRestController.java
@@ -1,9 +1,13 @@
 package study.dirk;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * Hello world!
@@ -11,14 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RefreshScope
 @RestController
-public class DirkRestController 
-{
+public class DirkRestController {
 	@Value("${server.port}")
 	private String value;
-	
+
 	@RequestMapping("config/get")
-    public String getConfig()
-    {
-        return value;
-    }
+	@HystrixCommand(fallbackMethod = "fallback")
+	public String getConfig() throws Exception {
+		Random rand = new Random();
+        //模拟错误百分比(方式比较粗鲁但可以证明问题)
+        if (1 == rand.nextInt(2)) {
+//            System.out.println("make exception");
+            throw new Exception("make exception");
+        }
+		return value;
+	}
+
+	@RequestMapping("config/get2")
+	@HystrixCommand(fallbackMethod = "fallback")
+	public String getConfig2() throws Exception {
+		Random rand = new Random();
+        //模拟错误百分比(方式比较粗鲁但可以证明问题)
+        if (1 == rand.nextInt(2)) {
+//            System.out.println("make exception");
+            throw new Exception("make exception");
+        }
+		return value;
+	}
+	
+	public String fallback() {
+		return "hystrix fallback";
+	}
 }
--- dirk-rest/src/main/java/study/dirk/DirkRestServerApplication.java
+++ dirk-rest/src/main/java/study/dirk/DirkRestServerApplication.java
@@ -2,10 +2,14 @@ package study.dirk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class DirkRestServerApplication {
 
 	public static void main(String[] args) {

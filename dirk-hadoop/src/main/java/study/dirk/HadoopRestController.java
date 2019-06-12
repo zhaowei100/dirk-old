d09e3bfacebdb2c95af93af5fb3897de96224738
wuti <linshi100q@163.com>
2019-06-12 23:05:51 +0800

    util

--- dirk-hadoop/src/main/java/study/dirk/HadoopRestController.java
+++ dirk-hadoop/src/main/java/study/dirk/HadoopRestController.java
@@ -1,27 +1,67 @@
 package study.dirk;
 
-import java.util.Random;
+import java.util.List;
 
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.hadoop.hbase.HbaseTemplate;
+import org.springframework.data.hadoop.hbase.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * Hello world!
  *
  */
 @RestController
+@RequestMapping()
 public class HadoopRestController {
 
 	@Autowired
 	private HbaseTemplate hbaseTemplate;
+
+	@RequestMapping("hbase/test")
+	public String putTest() throws Exception {
+		String tableName = "hadoop_test";
+		String rowName = "abc";
+		String familyName = "zhao";
+		String qualifier = "test";
+
+		hbaseTemplate.put(tableName, rowName, familyName, qualifier, "just test".getBytes());
+		return hbaseTemplate.get(tableName, rowName, familyName, qualifier, new RowMapper<String>() {
+
+			@Override
+			public String mapRow(Result result, int rowNum) throws Exception {
+				List<Cell> ceList = result.listCells();
+				if (ceList != null && ceList.size() > 0) {
+					for (Cell cell : ceList) {
+						return cell.getValue().toString();
+					}
+				}
+				return "";
+			}
+		});
+	}
 	
-	@RequestMapping("hbase/set")
-	public String getConfig() throws Exception {
-		hbaseTemplate.put(tableName, rowName, familyName, qualifier, value);
-		return "";
+	@RequestMapping("hbase/get")
+	public String getTest() throws Exception {
+		String tableName = "hadoop_test";
+		String rowName = "abc";
+		String familyName = "zhao";
+		String qualifier = "test";
+		return hbaseTemplate.get(tableName, rowName, familyName, qualifier, new RowMapper<String>() {
+
+			@Override
+			public String mapRow(Result result, int rowNum) throws Exception {
+				List<Cell> ceList = result.listCells();
+				if (ceList != null && ceList.size() > 0) {
+					for (Cell cell : ceList) {
+						return cell.getValue().toString();
+					}
+				}
+				return "";
+			}
+		});
 	}
 }
--- dirk-hadoop/src/main/java/study/dirk/hadoop/hbase/HBaseConfig.java
+++ dirk-hadoop/src/main/java/study/dirk/hadoop/hbase/HBaseConfig.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +16,17 @@ public class HBaseConfig {
 
     private final HBaseProperties properties;
 
+    @Value("${hadoop.home.dir}")
+	private String hadoop;
+    
     public HBaseConfig(HBaseProperties properties) {
         this.properties = properties;
     }
 
     @Bean
     public HbaseTemplate hbaseTemplate() {
+    	System.setProperty("hadoop.home.dir", hadoop);
+    	System.out.println(hadoop);
         HbaseTemplate hbaseTemplate = new HbaseTemplate();
         hbaseTemplate.setConfiguration(configuration());
         hbaseTemplate.setAutoFlush(true);
